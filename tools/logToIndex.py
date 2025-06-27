98fa8204b4a49be6e2564f32b1185b487d6e6fbb
bpsfty <46378561+bpsfty@users.noreply.github.com>
2025-06-27 11:12:53 +0530

    Update logToIndex.py for it run in k8s dev
    changes

--- tools/logToIndex.py
+++ tools/logToIndex.py
@@ -1,25 +1,37 @@
 from elasticsearch import Elasticsearch
 from datetime import datetime
+import logging
+import requests
+import json
+
+logging.basicConfig(level=logging.DEBUG)
 
 #indexName="brent_spot_price"
-fileName="./synthetic_app_generated.log"
-headers = {"Content-Type": "application/json"}
+fileName="/home/bhupinder_parmar/es/es-tools/tools/synthetic_app_generated.log"
+headers = {"Accept": "application/vnd.elasticsearch+json; compatible-with=8", "Content-Type": "application/vnd.elasticsearch+json; compatible-with=8"}
 es_user=""
 es_pwd=""
 es_url="http://10.131.206.48:31340"
 index_name = "synthetic-app-logs"
 # Connect to Elasticsearch
-#es = Elasticsearch("http://localhost:9200")  # Change to your host/port
-es = Elasticsearch(hosts=[es_url], basic_auth=(es_user,es_pwd), timeout=120 ) #Elasticsearch(es_url)
+#es = Elasticsearch("http://10.131.206.48:31340", headers={
+#  "Accept": "application/vnd.elasticsearch+json; compatible-with=8",
+#  "Content-Type": "application/vnd.elasticsearch+json; compatible-with=8"
+#})  # Change to your host/port
+#es = Elasticsearch(hosts=[es_url], verify_certs=False, basic_auth=(es_user,es_pwd), request_timeout=120 ) #Elasticsearch(es_url)
 
 #index_name = "app-logs"
 
 # Create index if it doesn't exist
-if not es.indices.exists(index=index_name):
-    es.indices.create(index=index_name)
+#idx =  es.indices.exists(index=index_name)
+#if (idx is None) or (not idx):
+#    es.indices.create(index=index_name)
 #es.indices.create(index=index_name)
-
+# Only once
+#response = requests.put(f"{es_url}/{index_name}", headers=headers)
+#print(response.status_code, response.text)
 # Parse and send logs
+ex=False
 with open(fileName, "r") as file:
     for line in file:
         try:
@@ -28,11 +40,17 @@ with open(fileName, "r") as file:
             level, message = rest.split("] ", 1)
 
             doc = {
-                "timestamp": datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S"),
+                "timestamp": datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S").isoformat(),
                 "level": level.strip(),
                 "message": message.strip()
             }
 
-            es.index(index=index_name, document=doc)
+            # es.index(index=index_name, document=doc)
+            response = requests.post(f"{es_url}/{index_name}/_doc", headers=headers, json=doc)
+            print(response.status_code, response.json())
         except Exception as e:
             print(f"Failed to process line: {line.strip()} - {e}")
+            ex=True
+            break
+        if ex: break
+
